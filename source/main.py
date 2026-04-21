9fdeeefee70b5f9f437f9b061adf26bee4b5e8d9
discpic-ron <granddriod1210@gmail.com>
2026-04-21 17:44:10 -0400

    Correct screen variable name in main.py
    Fix typo in screen.fill() method call.

--- source/main.py
+++ source/main.py
@@ -1,4 +1,7 @@
 import pygame
+from dealer import Dealer
+from player import Player 
+from card import Card
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
@@ -6,14 +9,21 @@ screen = pygame.display.set_mode((800,600))
 # constants
 running = True
 current_turn = "player"
+dealer = Dealer()
 
 # colors
 black = (0,0,0)
 
+def hand_value(hand):
+  value = 0
+  for card in hand:
+    value += card.get_value()
+  return value
+      
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
-  sreen.fill(black)
+  screen.fill(black)
   pygame.display.flip()
 pygame.quit()
